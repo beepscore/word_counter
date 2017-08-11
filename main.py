8d30e9e0cf31dd5beb63cb73f74d97425faf784c
beepscore <support@beepscore.com>
2017-08-11 13:01:48 -0700

    In main.py change input_filename

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from word_counter import counter
 # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 # if __name__ == '__main__':
 
-input_filename = "data/input/words1_test.txt"
+input_filename = "data/input/words.txt"
 output_filename = "data/output/counts.txt"
 
 counter.write_word_counts(input_filename, output_filename)
