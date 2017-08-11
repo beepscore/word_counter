921a4da53fb28a1a4aeab84f5817637aa60735a5
beepscore <support@beepscore.com>
2017-08-11 11:06:21 -0700

    Add test_get_word_counts_empty

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -6,6 +6,15 @@ from word_counter import counter
 
 class TestCounter(unittest.TestCase):
 
+    def test_get_word_counts_empty(self):
+
+        text = ""
+
+        # call method under test
+        actual = counter.get_word_counts(text)
+
+        self.assertEqual(actual, [])
+
     def test_get_word_counts(self):
 
         text = "foo bar baz foo bar bar baz bar"
